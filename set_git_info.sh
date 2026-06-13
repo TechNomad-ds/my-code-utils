014461fe32cf25e980b04ab20b076a8c82edcf09
TechNomad-ds <qfcai@stu.ecnu.edu.cn>
2026-06-13 21:55:46 +0800

    set git info shell

---
+++ set_git_info.sh
@@ -0,0 +1,5 @@
+# git config user.email "qfcai@stu.ecnu.edu.cn"
+# git config user.name "TechNomad-ds"
+
+git config --global user.email "qfcai@stu.ecnu.edu.cn"
+git config --global user.name "TechNomad-ds"
